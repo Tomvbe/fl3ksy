73f76d3aebd5fe189c9c7b2c8985ca17a4395511
Tom <tom.vanbesien@gmail.com>
2021-08-05 21:58:28 +0200

    Create spring service project

---
+++ service/src/main/kotlin/com/fl3ksy/service/ServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.fl3ksy.service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<ServiceApplication>(*args)
+}
---
+++ service/src/test/kotlin/com/fl3ksy/service/ServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fl3ksy.service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
