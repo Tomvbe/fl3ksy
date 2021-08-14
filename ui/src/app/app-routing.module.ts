23276710b5e05f24b5437c20cc1d0bec6de2343e
Tom <tom.vanbesien@gmail.com>
2021-08-14 22:04:20 +0200

    Add home screen

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'timer', pathMatch: 'full'}
+  { path: '', component: HomeComponent},
+  // { path: '', redirectTo: 'timer', pathMatch: 'full'}
 ];
 
 @NgModule({
