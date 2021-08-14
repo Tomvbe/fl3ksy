fc0414ae55f4404694cc55ce1b650d45731e6b9c
Tom <tom.vanbesien@gmail.com>
2021-08-14 22:47:37 +0200

    Implement test for localstorage

--- ui/src/app/activities/activities.component.ts
+++ ui/src/app/activities/activities.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActivitiesComponent implements OnInit {
 
+  clicksOnInit = localStorage.getItem('clicks');
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  clicked() {
+    const clicks = localStorage.getItem('clicks');
+
+    clicks != null
+      ? localStorage.setItem('clicks', (parseInt(clicks) + 1) + '')
+      : localStorage.setItem('clicks', '1');
+  }
 }
