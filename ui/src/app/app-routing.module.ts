7bf81f410b02bf87fb741c90f5819db35acbb674
Tom <tom.vanbesien@gmail.com>
2021-08-21 18:53:06 +0200

    Disable home component as it is useless at the moment

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {HomeComponent} from "./home/home.component";
 
 const routes: Routes = [
-  { path: '', component: HomeComponent},
+  { path: '', redirectTo: 'timer', pathMatch: 'full'},
   { path: 'activities', loadChildren: () => import('./activities/activities.module').then(m => m.ActivitiesModule) },
-  // { path: '', redirectTo: 'timer', pathMatch: 'full'}
 ];
 
 @NgModule({
